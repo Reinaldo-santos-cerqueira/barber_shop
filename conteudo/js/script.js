5d18047c57b9c282fc9b6b5fbb1d44db840e4e5f
Reinaldo Santos Cerqueira de Oliveira <kingnaldoreinaldo@gmail.com>
2021-03-23 23:00:52 -0300

    finalizado

--- conteudo/js/script.js
+++ conteudo/js/script.js
@@ -2,3 +2,10 @@ window.addEventListener("scroll",function(){
     var header  =   document.querySelector("header");
     header.classList.toggle("stiky",window.scrollY)
 })
+var ul_menu_mobile_open = document.getElementById("ul-menu-mobile");
+function menu_open(){
+    ul_menu_mobile_open.style.right = 0;
+}
+function menu_close(){
+    ul_menu_mobile_open.style.right = "-200vh";
+}
