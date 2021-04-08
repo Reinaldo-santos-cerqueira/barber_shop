58a6fa009cfc43dc8f56785b3833500cf3c20cc2
Reinaldo Santos Cerqueira de Oliveira <kingnaldoreinaldo@gmail.com>
2021-04-07 22:06:45 -0300

    ajustes

--- conteudo/js/script.js
+++ conteudo/js/script.js
@@ -8,4 +8,18 @@ function menu_open(){
 }
 function menu_close(){
     ul_menu_mobile_open.style.right = "-200vh";
+};
+const menu_items = document.querySelectorAll('#menu-desktop a[href^="#"');
+menu_items.forEach(item => {
+    item.addEventListener('click', scrollToIdOnClick);
+})
+function scrollToIdOnClick(event){
+    event.preventDefault();
+    const element = event.target;
+    const id = element.getAttribute('href');
+    const to = document.querySelector(id).offsetTop;
+    window.scroll({
+        top: to - 130,
+        behavior: "smooth",
+    });
 }
